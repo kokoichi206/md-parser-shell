2da180f78e190a4f9e459baf57acf7aa522d408f
takahiro.tominaga <tt.300607@gmail.com>
2022-02-12 15:10:48 +0900

    feat: h-tag detect and create output file (#1)

--- main.sh
+++ main.sh
@@ -27,7 +27,38 @@ if [ ! -f "$1" ]; then
     print_error_and_usage_and_exit "File $1 doesn't exists"
 fi
 
+found_title=false
+line_count=0
 while read line
 do
-    echo "$line"
+    line_count=$(($line_count + 1))
+
+    # ===== h-tag =====
+    is_h_line=true
+    h_tag_num=$((`echo "$line" | sed -r "s/^(#*) .*/\1/g" | sed "s/^[^#].*//g" | wc -c` - 1))
+    case "$h_tag_num" in
+        0)
+            is_h_line=false
+            ;;
+        1)
+            if [ -n "$h1" ]; then
+                echo "Detected another h1-tag at line:$line_count"
+                echo "Please use only one h1-tag"
+                exit 1
+            fi
+            # need check file existance??
+            h1=`echo $line | sed "s/^#* //g"`
+            # Create output html file.
+            touch "${h1}.html"
+            ;;
+        2)
+            h2=`echo $line | sed "s/^#* //g"`
+            ;;
+        3)
+            h3=`echo $line | sed "s/^#* //g"`
+            ;;
+        *)
+            h4_or_more=`echo $line | sed "s/^#* //g"`
+            ;;
+    esac
 done < $1
